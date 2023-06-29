f79d64328c28baa1989bc4fb4c3129d92c94238f
RexYuan <r1218r1218@gmail.com>
2023-06-29 23:18:54 +0800

    Updated to latest api and bash

---
+++ ddns.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+. secret.sh
+
+CURRENT_IP=$(curl -s http://ipv4.icanhazip.com)
+DNS_IP=$(dig +short "${URLS[0]}" | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | awk 'NR==1{print}')
+
+if [ "$CURRENT_IP" != "$DNS_IP" ]; then
+    echo "Renew IP: $DNS_IP to $CURRENT_IP"
+    for ((i = 0; i < ${#URLS[@]}; i++)); do
+        curl --request PUT \
+            --url "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/${RECORD_IDS[$i]}" \
+            --header "Content-Type: application/json" \
+            --header "X-Auth-Email: $EMAIL" \
+            --header "Authorization: Bearer $KEY" \
+            --data "{
+        \"type\": \"A\",
+        \"name\": \"${URLS[$i]}\",
+        \"content\": \"$CURRENT_IP\"
+        }"
+    done
+else
+    echo "No change: $CURRENT_IP"
+fi
